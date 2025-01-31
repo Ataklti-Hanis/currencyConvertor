a7afdff5dd6a770477c03b702300e6f392018442
AtakltiHanis <atakltihanis14@gmail.com>
2025-01-31 23:51:58 +0300

    being...

--- src/App.js
+++ src/App.js
@@ -6,27 +6,40 @@ export default function App() {
   const [amount, setAmount] = useState(1);
   const [fromCurrency, setFromCurrency] = useState("EUR");
   const [toCurrency, setToCurrency] = useState("USD");
+  const [exchangeRate, setExchangeRate] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(function () {
-    async function convert() {
-      const response = await fetch(
-        `https://api.frankfurter.app/latest?amount=${amount}$from=${fromCurrency}&to=${toCurrency}`
-      );
-      const data = await response.json();
-      console.log(data.rates[toCurrency]);
-    }
-    convert();
-  }, []);
+  useEffect(
+    function () {
+      async function convert() {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://api.frankfurter.app/latest?from=${fromCurrency}&to=${toCurrency}`
+        );
+        const data = await response.json();
+        setExchangeRate(data.rates[toCurrency]);
+        setIsLoading(false);
+      }
+      if (fromCurrency === toCurrency) {
+        setExchangeRate(amount);
+        return;
+      }
+      convert();
+    },
+    [amount, fromCurrency, toCurrency]
+  );
   return (
     <div className="App">
       <input
         type="text"
         value={amount}
         onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
       />
       <select
         value={fromCurrency}
-        onChange={(e) => setFromCurrency(Number(e.target.value))}
+        onChange={(e) => setFromCurrency(e.target.value)}
+        disabled={isLoading}
       >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
@@ -36,13 +49,18 @@ export default function App() {
       <select
         value={toCurrency}
         onChange={(e) => setToCurrency(Number(e.target.value))}
+        disabled={isLoading}
       >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>
+        {isLoading
+          ? "Loading..."
+          : `1 ${fromCurrency} = ${exchangeRate} ${toCurrency}`}
+      </p>
     </div>
   );
 }
